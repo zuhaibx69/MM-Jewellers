4c85406ec09f77d6e654cf41490f97e73eaabd6e
Muhammad Zohaib <128610225+Zohaib-x9@users.noreply.github.com>
2023-04-14 02:01:59 +0500

    Push Project

---
+++ MM Jewellers/Controllers/WholesalerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MM_Jewellers.Controllers
+{
+    public class WholesalerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ MM Jewellers/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MM_Jewellers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
