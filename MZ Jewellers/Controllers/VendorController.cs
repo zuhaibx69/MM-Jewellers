1fad6d51355367063936913c92ee15bf8c1a986a
Muhammad Zohaib <128610225+Zohaib-x9@users.noreply.github.com>
2023-04-27 02:06:41 +0500

    Update Vendor Module

--- MZ Jewellers/Controllers/VendorController.cs
+++ MZ Jewellers/Controllers/VendorController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MZ_Jewellers.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,33 @@ namespace MZ_Jewellers.Controllers
 {
     public class VendorController : Controller
     {
-        
+        JewelleryManagementSystemEntities db = new JewelleryManagementSystemEntities();
+
         // GET: Vendor
+
+        [HttpGet]
         public ActionResult Index()
         {
+            ViewBag.QRlist = db.Quotation_Request.ToList();
+            ViewBag.Prodlist = db.Products.ToList();
+            Quotation_Request s = new Quotation_Request();
+            string a = s.order_deadline.Date.ToString();
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Index(int price_perunit)
+        {
+            ViewBag.QRlist = db.Quotation_Request.ToList();
+            ViewBag.Prodlist = db.Products.ToList();
+
+            Quotation_Response qr = new Quotation_Response();
+            qr.vendor_id = 1;
+            qr.price_perunit = price_perunit;
+            qr.req_id = 1;
+
+            return View();
+        }
+
     }
 }
